b49a946f97ee6a44c279744c168fa2b246fb1680
vinatgit <vinayakaggarwal97@gmail.com>
2017-10-07 23:55:30 +0530

    Starting views

--- application/trafficontrol/models.py
+++ application/trafficontrol/models.py
@@ -10,7 +10,9 @@ class Vehicle(models.Model):
 	penalty = models.IntegerField("Penalty Dues",default = 0)
 	lastcrossing_lat = models.FloatField("Last seen latitude", default = -1.0)
 	lastcrossing_long = models.FloatField("Last seen longitude", default  = -1.0)
-	
+	missing = models.BooleanField("Missing", default = False)
+	missing_when = models.DateTimeField("Time of Missing Report", default = None)
+
 '''
 class Penalty(models.Model):#Include type of offence in a set
 	veh_number = models.ForeignKey(Vehicle)
@@ -20,23 +22,31 @@ class Penalty(models.Model):#Include type of offence in a set
 	when = models.DateTimeField()
 '''
 
-class Charges(models.Model):#Implement charge nature in a set
+class Charges(models.Model):
+	charge_kind_choice = (
+			("TC", "Toll Charge"),
+			("TV", "Traffic Light Violation"),
+			("OS", "Overspeeding"),
+			)	
+
 	veh_number = models.ForeignKey(Vehicle)
 	charge_type = models.BooleanField("Offence")
-	charge_kind = models.CharField("Charge nature", max_length = 30)
+	charge_kind = models.CharField("Charge nature", max_length = 2, choices = charge_kind_choice)
         charge = models.CharField("Charges", max_length = 100)        
 	loc_lat = models.FloatField("Latitude")
         loc_long = models.FloatField("Longitude") 
         when = models.DateTimeField("Date of Charge")
 
-class Missing(models.Model):
-	veh_number = models.ForeignKey(Vehicle)
-	when = models.DateTimeField("Date of Filing Report")
-
 class Payment(models.Model): #Implement payment mode in a set
+	payment_choice = (
+			("CA", "Cash"),
+			("NB", "Net Banking"),
+			("OW", "Online Wallet"),
+			)
+
 	veh_number = models.ForeignKey(Vehicle)
 	payment_type = models.BooleanField("Offence")
-	payment_mode = models.CharField("Mode of Payment", max_length = 20)
+	payment_mode = models.CharField("Mode of Payment", max_length = 2, choices = payment_choice)
 	when = models.DateTimeField("Date of Payment")
 
 '''
--- application/trafficontrol/views.py
+++ application/trafficontrol/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("YOYO")
 
+def missing(request):
+	if request.POST.get('vehicle') in Vehicle.objects.filter(missing = True):
+		return
+
+def add_charge(request):
+		 
 # Create your views here.
