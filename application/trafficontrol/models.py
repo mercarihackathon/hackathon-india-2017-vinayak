131bfff9c208a9b2ee75d1d94703050f84e210f8
vinatgit <vinayakaggarwal97@gmail.com>
2017-10-07 22:57:54 +0530

    Models refined

--- application/trafficontrol/models.py
+++ application/trafficontrol/models.py
@@ -6,27 +6,41 @@ class Vehicle(models.Model):
 	veh_type = models.CharField("Type of Vehicle", max_length = 10)
 	veh_make = models.CharField("Make of Vehicle", max_length = 30)
 	owner_id = models.IntegerField("Identification of Owner") #Aadhar for India
-	road_charges = models.IntegerField("Charges due", default = 0)
-	penalty = models.IntegerField("Fines due", default = 0)
+	charges = models.IntegerField("Charges Dues",default = 0)
+	penalty = models.IntegerField("Penalty Dues",default = 0)
 	lastcrossing_lat = models.FloatField("Last seen latitude", default = -1.0)
 	lastcrossing_long = models.FloatField("Last seen longitude", default  = -1.0)
 	
-
+'''
 class Penalty(models.Model):#Include type of offence in a set
 	veh_number = models.ForeignKey(Vehicle)
 	offence = models.CharField("Offence", max_length = 100)
 	loc_lat = models.FloatField("Latitude")
         loc_long = models.FloatField("Longitude")
 	when = models.DateTimeField()
+'''
 
-class Charges(models.Model):#Include location of tolls
+class Charges(models.Model):#Implement charge nature in a set
 	veh_number = models.ForeignKey(Vehicle)
+	charge_type = models.BooleanField("Offence")
+	charge_kind = models.CharField("Charge nature", max_length = 30)
         charge = models.CharField("Charges", max_length = 100)        
 	loc_lat = models.FloatField("Latitude")
         loc_long = models.FloatField("Longitude") 
-        when = models.DateTimeField()
+        when = models.DateTimeField("Date of Charge")
 
+class Missing(models.Model):
+	veh_number = models.ForeignKey(Vehicle)
+	when = models.DateTimeField("Date of Filing Report")
+
+class Payment(models.Model): #Implement payment mode in a set
+	veh_number = models.ForeignKey(Vehicle)
+	payment_type = models.BooleanField("Offence")
+	payment_mode = models.CharField("Mode of Payment", max_length = 20)
+	when = models.DateTimeField("Date of Payment")
+
+'''
 class Crossings(models.Model):
 	loc_lat = models.FloatField("Latitude")
         loc_long = models.FloatField("Longitude")
-	
+'''	
