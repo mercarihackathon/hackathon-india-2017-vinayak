6dca20570d109d971796f2776a1b9b23baa06089
vinatgit <vinayakaggarwal97@gmail.com>
2017-10-08 07:48:21 +0530

    Starting views 2

--- application/trafficontrol/models.py
+++ application/trafficontrol/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Vehicle(models.Model):
-<<<<<<< HEAD
 	veh_number = models.CharField("Number Plate of Vehicle", max_length = 10,  primary_key = True)
 	veh_color = models.CharField("Color of Vehicle", max_length = 10)
 	veh_type = models.CharField("Type of Vehicle", max_length = 10)
@@ -13,19 +12,6 @@ class Vehicle(models.Model):
 	lastcrossing_long = models.FloatField("Last seen longitude", default  = -1.0)
 	missing = models.BooleanField("Missing", default = False)
 	missing_when = models.DateTimeField("Time of Missing Report", default = None)
-
-=======
-    veh_number = models.CharField("Number Plate of Vehicle", max_length = 10,  primary_key = True)
-    veh_color = models.CharField("Color of Vehicle", max_length = 10)
-    veh_type = models.CharField("Type of Vehicle", max_length = 10)
-    veh_make = models.CharField("Make of Vehicle", max_length = 30)
-    owner_id = models.IntegerField("Identification of Owner") #Aadhar for India
-    charges = models.IntegerField("Charges Dues",default = 0)
-    penalty = models.IntegerField("Penalty Dues",default = 0)
-    lastcrossing_lat = models.FloatField("Last seen latitude", default = -1.0)
-    lastcrossing_long = models.FloatField("Last seen longitude", default  = -1.0)
-	
->>>>>>> fd79596c5e7bdd51ca9230cd22440fdbf46f2704
 '''
 class Penalty(models.Model):#Include type of offence in a set
     veh_number = models.ForeignKey(Vehicle)
@@ -35,9 +21,8 @@ class Penalty(models.Model):#Include type of offence in a set
     when = models.DateTimeField()
 '''
 
-<<<<<<< HEAD
 class Charges(models.Model):
-	charge_kind_choice = (
+	charge_choice = (
 			("TC", "Toll Charge"),
 			("TV", "Traffic Light Violation"),
 			("OS", "Overspeeding"),
@@ -45,8 +30,7 @@ class Charges(models.Model):
 
 	veh_number = models.ForeignKey(Vehicle)
 	charge_type = models.BooleanField("Offence")
-	charge_kind = models.CharField("Charge nature", max_length = 2, choices = charge_kind_choice)
-        charge = models.CharField("Charges", max_length = 100)        
+	charge = models.CharField("Charging nature", max_length = 2, choices = charge_choice)
 	loc_lat = models.FloatField("Latitude")
         loc_long = models.FloatField("Longitude") 
         when = models.DateTimeField("Date of Charge")
@@ -61,27 +45,8 @@ class Payment(models.Model): #Implement payment mode in a set
 	veh_number = models.ForeignKey(Vehicle)
 	payment_type = models.BooleanField("Offence")
 	payment_mode = models.CharField("Mode of Payment", max_length = 2, choices = payment_choice)
+	amount = models.FloatField("Amount Paid")
 	when = models.DateTimeField("Date of Payment")
-=======
-class Charges(models.Model):#Implement charge nature in a set
-    veh_number = models.ForeignKey(Vehicle)
-    charge_type = models.BooleanField("Offence")
-    charge_kind = models.CharField("Charge nature", max_length = 30)
-    charge = models.CharField("Charges", max_length = 100)        
-    loc_lat = models.FloatField("Latitude")
-    loc_long = models.FloatField("Longitude") 
-    when = models.DateTimeField("Date of Charge")
-
-class Missing(models.Model):
-    veh_number = models.ForeignKey(Vehicle)
-    when = models.DateTimeField("Date of Filing Report")
-
-class Payment(models.Model): #Implement payment mode in a set
-    veh_number = models.ForeignKey(Vehicle)
-    payment_type = models.BooleanField("Offence")
-    payment_mode = models.CharField("Mode of Payment", max_length = 20)
-    when = models.DateTimeField("Date of Payment")
->>>>>>> fd79596c5e7bdd51ca9230cd22440fdbf46f2704
 
 '''
 class Crossings(models.Model):
--- application/trafficontrol/views.py
+++ application/trafficontrol/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("YOYO")
+Toll = 0
+LV_charge = 2000
+OS_charge = 5000
 
-def missing(request):
-	if request.POST.get('vehicle') in Vehicle.objects.filter(missing = True):
-		return
+def index(request):
+    return HttpResponse("YOYO")
+    
+def alert_police(request):
+    cont = 1
+    missing_obj = Vehicle.objects.filter(missing = True)
+    missing_veh = [x.veh_number for x in missing_obj]
+    if request.POST.get('vehicle') in missing_veh:
+        cont = 0 #Alert police for a theft
+    elif len(Vehicle.objects.filter(veh_number = 'vehicle', veh_color = 'color', veh_type = 'type', veh_make = 'make')) == 0:
+        cont = 0 #Alert police for some discrepancy
+    return cont
 
 def add_charge(request):
-		 
+    cont = alert_police(request)
+    if cont:
+        #veh_number, charge, loc_lat, loc_long, when
+        
+        
 # Create your views here.
